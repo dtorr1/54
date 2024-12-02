993fb2a7ed373cf19abc15c3a4182992740e4a07
dtorr1 <dtorres69@hawkmail.hccfl.edu>
2024-12-02 01:03:47 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+public class Main {
+  public static void main(String[] args) {
+    // Create a constant number of kilometers in a mile
+    final double KILOMETERS_PER_MILE = 1.609; 
+    // Display table header
+    System.out.println(
+      "Miles        Kilometers");
+    // Create and display table showing conversion from miles to kilometers
+    for (int i = 1; i <= 10; i++) {
+      System.out.printf(
+        "%-13d%-10.3f\n", i, (i * KILOMETERS_PER_MILE));
+    }
+  }
+}
